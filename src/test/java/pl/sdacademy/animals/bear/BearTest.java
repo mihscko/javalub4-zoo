4704ad0006e4f1c6ea61415195a9c3afc073a9aa
mihscko <m.mazurek85@gmail.com>
2017-12-08 19:50:33 +0100

    Third method of BearTest

--- src/test/java/pl/sdacademy/animals/bear/BearTest.java
+++ src/test/java/pl/sdacademy/animals/bear/BearTest.java
@@ -1,7 +1,9 @@
 package pl.sdacademy.animals.bear;
 
+import org.joda.time.DateTime;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import pl.sdacademy.animals.time.Clock;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,9 +11,8 @@ class BearTest {
     @Test
     void BearShouldBeAliveImmediatelyAfterCreation() {
         int weight = 3;
-    Bear bear = new BlackBear(weight);
-    boolean result = bear.isAlive();
-    Assert.assertTrue(result);
+        Bear bear = new BlackBear(weight);
+        assertTrue(bear.isAlive());
     }
 
     @Test
@@ -20,11 +21,33 @@ class BearTest {
         Bear bear = new BlackBear(weight);
         boolean result = bear.isAlive();
         bear.eat();
-        Assert.assertTrue(result);
+        assertTrue(bear.isAlive());
     }
 
     @Test
-    void getWeight() {
+    void BearShouldNotBeAliveIfItHasEatenWithinMoreThanTenDays() {
+        int weight = 3;
+        TestClock clock = new TestClock();
+        Bear bear = new BlackBear(weight, clock);
+        bear.eat();
+        clock.changeTimeByDays(10);
+        boolean result = bear.isAlive();
+
+        Assert.assertTrue(result == false);
+
     }
 
+    class TestClock implements Clock {
+
+        private DateTime time = DateTime.now();
+
+        @Override
+        public DateTime getCurrentTime() {
+            return time;
+        }
+
+        public void changeTimeByDays(int days) {
+            time = time.plusDays(days);
+        }
+    }
 }
